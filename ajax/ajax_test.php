901e5e75a692120b6ef2d35b8e9429ab89829ec7
Wan Lun,Zheng <s1015346@gm.pu.edu.tw>
2018-12-17 23:17:17 +0800

    新增ajax
    Signed-off-by: Wan Lun,Zheng <s1015346@gm.pu.edu.tw>

---
+++ ajax/ajax_test.php
@@ -0,0 +1,9 @@
+<?php
+$data = array('a' => 'apple', 'b' => 'banana', 'c' => 'carambola', 'd' => 'durian');
+$data = array('apple','banana','durian');
+
+$data_json = json_encode($data);
+echo $data_json;
+//'a' => 'apple', 'b' => 'banana', 'c' => 'carambola', 'd' => 'durian'
+?>
+
